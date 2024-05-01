471aa37effb70e19a05d344be6e468846b8ac53e
Quoc-Toan Ly <104543062+toan-ly@users.noreply.github.com>
2024-05-01 13:25:01 -0400

    Update ImProcessing.m

--- MATLAB/ImProcessing.m
+++ MATLAB/ImProcessing.m
@@ -1,17 +1,17 @@
 mean_subtract_val = [103.939, 116.779, 123.68];
 
 %% Store Doraemon
-dataPath = 't:\datatransfer\Interns\Toan\Lessons\9-TransferLearning\Images\Doraemon';
-outPath =  't:\datatransfer\Interns\Toan\Lessons\9-TransferLearning\Data';
+dataPath = 'Images\Doraemon';
+outPath =  'Data';
 StoreImages(dataPath, outPath, mean_subtract_val, 1);
 
 %% Store Oggy
-dataPath = 't:\datatransfer\Interns\Toan\Lessons\9-TransferLearning\Images\Oggy';
+dataPath = 'Images\Oggy';
 StoreImages(dataPath, outPath, mean_subtract_val, 0);
 
 %% Split into Train and Val
-trainPath = 't:\datatransfer\Interns\Toan\Lessons\9-TransferLearning\Data\Train';
-valPath = 't:\datatransfer\Interns\Toan\Lessons\9-TransferLearning\Data\Val';
+trainPath = 'Data\Train';
+valPath = 'Data\Val';
 dataPath = outPath;
 SplitData(dataPath, trainPath, valPath);
 
